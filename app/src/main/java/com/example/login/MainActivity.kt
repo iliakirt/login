cd5470be9533a05458cf7aa93b9d8d344cceacb2
Ilias Kirtsiotis <iliakirt@gmail>
2021-11-26 11:56:18 +0200

    loginform

---
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.login
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.login.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.loginbtn.setOnClickListener{ loginTip() }
+
+    }
+    private fun loginTip() {
+        val username = binding.username.text.toString()
+        val password = binding.password.text.toString()
+        if (username.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please fill both fields", Toast.LENGTH_LONG).show()
+        }else if (username.length != 11){
+            Toast.makeText(this, "AMKA must be 11 digits", Toast.LENGTH_LONG).show()
+        }
+
+
+
+
+    }
+}
+
+
