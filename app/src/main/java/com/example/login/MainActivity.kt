d51d81b26944050d58a637cfd916058d3e56b72e
Ilias Kirtsiotis <iliakirt@gmail>
2021-11-27 17:45:01 +0200

    login_form

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -1,33 +1,36 @@
 package com.example.login
 
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.login.databinding.ActivityMainBinding
 
-
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.loginbtn.setOnClickListener{ loginTip() }
-
+        binding.loginbtn.setOnClickListener{ login() }
     }
-    private fun loginTip() {
+    private fun login() {
+        val user = "12345678901"
+        val pass = "admin"
         val username = binding.username.text.toString()
         val password = binding.password.text.toString()
         if (username.isEmpty() || password.isEmpty()) {
             Toast.makeText(this, "Please fill both fields", Toast.LENGTH_LONG).show()
-        }else if (username.length != 11){
+        } else if (username.length != 11) {
             Toast.makeText(this, "AMKA must be 11 digits", Toast.LENGTH_LONG).show()
+        }else {
+            if (username == user && password == pass){
+                Toast.makeText(this, "Login Successfully", Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(this, "incorrect AMKA or password", Toast.LENGTH_LONG).show()
+            }
         }
-
     }
 }
 
